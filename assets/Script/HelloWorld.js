7559ba6d985e202c5da96f868e73fd38a65ba01b
knox <knox.h@qq.com>
2016-07-12 18:24:47 +0800

    Update HelloWorld.js
    fix for Script caching issue #889

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -6,6 +6,7 @@ cc.Class({
             default: null,
             type: cc.Label
         },
+        // defaults, set visually when attaching this script to the Canvas
         text: 'Hello, World!'
     },
 
